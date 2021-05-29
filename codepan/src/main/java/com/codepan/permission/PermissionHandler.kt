21a01364915ed5612a98dc0ef4209119ed273853
jeffreyconcha <depanconcha@gmail.com>
2021-05-29 18:59:55 +0800

    New permission type

--- codepan/src/main/java/com/codepan/permission/PermissionHandler.kt
+++ codepan/src/main/java/com/codepan/permission/PermissionHandler.kt
@@ -1,6 +1,5 @@
 package com.codepan.permission
 
-import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -21,46 +20,8 @@ enum class PermissionType(val value: PermissionValue) {
     PHONE_NUMBERS(PermissionValue.phoneNumbers),
     READ_CALL_LOG(PermissionValue.readCallLog),
     READ_SMS(PermissionValue.readSms),
-}
-
-class PermissionValue(vararg values: String) {
-
-    val permissions = values
-
-    companion object {
-        @RequiresApi(Build.VERSION_CODES.Q)
-        val backgroundLocation = PermissionValue(
-            Manifest.permission.ACCESS_BACKGROUND_LOCATION
-        );
-        val foregroundLocation = PermissionValue(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION
-        )
-        val filesAndMedia = PermissionValue(
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE
-        )
-        val camera = PermissionValue(
-            Manifest.permission.CAMERA
-        );
-        val microphone = PermissionValue(
-            Manifest.permission.RECORD_AUDIO
-        );
-        val readCallLog = PermissionValue(
-            Manifest.permission.READ_CALL_LOG,
-            Manifest.permission.READ_PHONE_STATE,
-        );
-        val readSms = PermissionValue(
-            Manifest.permission.READ_SMS
-        );
-        val phoneNumbers: PermissionValue
-            get() {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    return PermissionValue(Manifest.permission.READ_PHONE_NUMBERS);
-                }
-                return PermissionValue(Manifest.permission.READ_PHONE_STATE);
-            }
-    }
+    RECEIVE_SMS(PermissionValue.receiveSms),
+    SEND_SMS(PermissionValue.sendSms),
 }
 
 interface PermissionEvents {
---
+++ codepan/src/main/java/com/codepan/permission/PermissionValue.kt
@@ -0,0 +1,51 @@
+package com.codepan.permission
+
+import android.Manifest
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+class PermissionValue(vararg values: String) {
+
+    val permissions = values
+
+    companion object {
+        @RequiresApi(Build.VERSION_CODES.Q)
+        val backgroundLocation = PermissionValue(
+            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+        );
+        val foregroundLocation = PermissionValue(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+        val filesAndMedia = PermissionValue(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+        val camera = PermissionValue(
+            Manifest.permission.CAMERA
+        );
+        val microphone = PermissionValue(
+            Manifest.permission.RECORD_AUDIO
+        );
+        val readCallLog = PermissionValue(
+            Manifest.permission.READ_CALL_LOG,
+            Manifest.permission.READ_PHONE_STATE,
+        );
+        val readSms = PermissionValue(
+            Manifest.permission.READ_SMS
+        );
+        val phoneNumbers: PermissionValue
+            get() {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    return PermissionValue(Manifest.permission.READ_PHONE_NUMBERS);
+                }
+                return PermissionValue(Manifest.permission.READ_PHONE_STATE);
+            }
+        val receiveSms = PermissionValue(
+            Manifest.permission.READ_SMS
+        );
+        val sendSms = PermissionValue(
+            Manifest.permission.SEND_SMS
+        );
+    }
+}
