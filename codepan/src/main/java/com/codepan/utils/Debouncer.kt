eafb51602cff466fbef6b2d31f1cf4ba648e1dbb
jeffreyconcha <depanconcha@gmail.com>
2021-08-16 17:19:41 +0800

    Debouncer

--- codepan/src/main/java/com/codepan/utils/Debouncer.kt
+++ codepan/src/main/java/com/codepan/utils/Debouncer.kt
@@ -3,14 +3,15 @@ package com.codepan.utils
 import java.util.*
 
 class Debouncer(val delay: Long = 500L) {
-    private val timer = Timer()
+    private var timer: Timer? = null;
 
     fun run(task: TimerTask) {
-        timer.cancel()
-        timer.schedule(task, delay);
+        timer?.cancel()
+        timer = Timer();
+        timer!!.schedule(task, delay);
     }
 
     fun cancel() {
-        timer.cancel()
+        timer?.cancel()
     }
 }
