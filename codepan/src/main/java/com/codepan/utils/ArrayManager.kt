00f91faa19de0c22814c91963d6f6009859c7d18
jeffreyconcha <depanconcha@gmail.com>
2021-11-24 14:37:30 +0800

    'b'

--- codepan/src/main/java/com/codepan/utils/ArrayManager.kt
+++ codepan/src/main/java/com/codepan/utils/ArrayManager.kt
@@ -2,6 +2,19 @@ package com.codepan.utils
 
 class ListManager<T>(val itemList: ArrayList<T>) {
 
+    val duplicate: T?
+        get() {
+            val holder = arrayListOf<T>()
+            for (item in itemList) {
+                if (holder.contains(item)) {
+                    return item
+                } else {
+                    holder.add(item)
+                }
+            }
+            return null;
+        };
+
     fun removeDuplicate(): ArrayList<T> {
         val holder = arrayListOf<T>()
         for (item in itemList) {
@@ -14,17 +27,6 @@ class ListManager<T>(val itemList: ArrayList<T>) {
         return itemList
     }
 
-    fun getDuplicate(): T? {
-        val holder = arrayListOf<T>()
-        for (item in itemList) {
-            if (holder.contains(item)) {
-                return item
-            } else {
-                holder.add(item)
-            }
-        }
-        return null
-    }
 
     companion object {
         fun <T> fromArray(array: Array<T>): ListManager<T> {
