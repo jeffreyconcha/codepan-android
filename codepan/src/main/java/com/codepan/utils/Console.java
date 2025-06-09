b70d63b73509af5ba6afba96c96dd72c4c6fecf1
Jeffrey Concha <depanconcha@gmail.com>
2025-06-09 23:18:51 +0800

    b

--- codepan/src/main/java/com/codepan/utils/Console.java
+++ codepan/src/main/java/com/codepan/utils/Console.java
@@ -40,13 +40,13 @@ public class Console {
 	}
 
 	public static void log(Object... inputs) {
-		for (Object input : inputs) {
+		for(Object input : inputs) {
 			info(input);
 		}
 	}
 
 	public static void log(Object input, boolean isDebug) {
-		if (isDebug) {
+		if(isDebug) {
 			info(input);
 		}
 	}
@@ -56,57 +56,65 @@ public class Console {
 	}
 
 	public static void logParams(String params) {
-		try {
-			JSONObject json = new JSONObject(params);
-			largeLog(PARAMS, json.toString(INDENT));
-		}
-		catch(JSONException je) {
-			je.printStackTrace();
+		if(params != null) {
 			try {
-				JSONArray array = new JSONArray(params);
-				largeLog(PARAMS, array.toString(INDENT));
+				JSONObject json = new JSONObject(params);
+				largeLog(PARAMS, json.toString(INDENT));
 			}
-			catch(JSONException e) {
-				e.printStackTrace();
-				largeLog(PARAMS, params);
+			catch(JSONException je) {
+				je.printStackTrace();
+				try {
+					JSONArray array = new JSONArray(params);
+					largeLog(PARAMS, array.toString(INDENT));
+				}
+				catch(JSONException e) {
+					e.printStackTrace();
+					largeLog(PARAMS, params);
+				}
 			}
 		}
 	}
 
 	public static void logResponse(String response) {
-		try {
-			JSONObject json = new JSONObject(response);
-			largeLog(RESPONSE, json.toString(INDENT));
-		}
-		catch(JSONException je) {
-			je.printStackTrace();
+		if(response != null) {
 			try {
-				JSONArray array = new JSONArray(response);
-				largeLog(RESPONSE, array.toString(INDENT));
+				JSONObject json = new JSONObject(response);
+				largeLog(RESPONSE, json.toString(INDENT));
 			}
-			catch(JSONException e) {
-				e.printStackTrace();
-				largeLog(RESPONSE, response);
+			catch(JSONException je) {
+				je.printStackTrace();
+				try {
+					JSONArray array = new JSONArray(response);
+					largeLog(RESPONSE, array.toString(INDENT));
+				}
+				catch(JSONException e) {
+					e.printStackTrace();
+					largeLog(RESPONSE, response);
+				}
 			}
 		}
 	}
 
 	public static void largeLog(String tag, String content) {
-		if(content.length() > CHAR_LIMIT) {
-			appendLog(tag, content.substring(0, CHAR_LIMIT));
-			appendLog(content.substring(CHAR_LIMIT));
-		}
-		else {
-			appendLog(tag, content);
+		if(content != null) {
+			if(content.length() > CHAR_LIMIT) {
+				appendLog(tag, content.substring(0, CHAR_LIMIT));
+				appendLog(content.substring(CHAR_LIMIT));
+			}
+			else {
+				appendLog(tag, content);
+			}
 		}
 	}
 
 	private static void appendLog(String content) {
-		if (content.length() > CHAR_LIMIT) {
-			appendLog(null, content.substring(0, CHAR_LIMIT));
-		}
-		else {
-			appendLog(null, content);
+		if(content != null) {
+			if(content.length() > CHAR_LIMIT) {
+				appendLog(null, content.substring(0, CHAR_LIMIT));
+			}
+			else {
+				appendLog(null, content);
+			}
 		}
 	}
 
