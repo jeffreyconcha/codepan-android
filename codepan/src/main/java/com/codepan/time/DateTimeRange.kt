13b6a67e7a3529b62f3cca7a515a33a7d7a85573
Jeffrey Concha <depanconcha@gmail.com>
2025-08-26 16:59:23 +0800

    b

--- codepan/src/main/java/com/codepan/time/DateTimeRange.kt
+++ codepan/src/main/java/com/codepan/time/DateTimeRange.kt
@@ -1,5 +1,8 @@
 package com.codepan.time
 
+import android.content.Context
+import com.codepan.R
+
 class DateTimeRange(
     val start: DateTime,
     val end: DateTime,
@@ -18,11 +21,32 @@ class DateTimeRange(
         LAST_3_MONTHS,
         LAST_6_MONTHS,
         THIS_YEAR,
-        FROM_THE_BEGINNING,
+        FROM_THE_BEGINNING;
+
+        fun getName(ctx: Context): String {
+            return when (this) {
+                TODAY -> ctx.getString(R.string.today)
+                YESTERDAY -> ctx.getString(R.string.yesterday)
+                THIS_WEEK -> ctx.getString(R.string.this_week)
+                THIS_MONTH -> ctx.getString(R.string.this_month)
+                LAST_WEEK -> ctx.getString(R.string.last_week)
+                LAST_MONTH -> ctx.getString(R.string.last_month)
+                LAST_7_DAYS -> ctx.getString(R.string.last_7_days)
+                LAST_30_DAYS -> ctx.getString(R.string.last_30_days)
+                LAST_3_MONTHS -> ctx.getString(R.string.last_3_months)
+                LAST_6_MONTHS -> ctx.getString(R.string.last_6_months)
+                THIS_YEAR -> ctx.getString(R.string.this_year)
+                FROM_THE_BEGINNING -> ctx.getString(R.string.from_the_beginning)
+            }
+        }
     }
 
     override fun toString(): String {
-        return "$period: ${start.date} - ${end.date}"
+        return "${period ?: "Custom"}: ${start.date} - ${end.date}"
+    }
+
+    fun getDisplayName(ctx: Context): String {
+        return "${period?.getName(ctx) ?: ctx.getString(R.string.custom)} (${start.readableDate} - ${end.readableDate})"
     }
 
     companion object {
---
+++ codepan/src/main/java/com/codepan/utils/R.kt
@@ -0,0 +1,5 @@
+package com.codepan.utils
+
+class R {
+
+}
